541dc1ecc6b02134823215b47974f4bee42d45c2
GTRsdk <rsmatthewcom.info@gmail.com>
2012-04-12 02:28:39 -0500

    Added .php to the links in menu.php so that should fix the links

--- www/includes/templates/menu.php
+++ www/includes/templates/menu.php
@@ -2,49 +2,49 @@
 <div id="header">
 		<ul id="nav">
 
-		<li><a href="/index">Home</a></li>
+		<li><a href="/index.php">Home</a></li>
 
         <?php
         if(!$cookieValid){
                 //Display this menu if the user isn't logged in
 	?>
-			<li><a href="/register">Register</a></li>
+			<li><a href="/register.php">Register</a></li>
 	<?php
 	} else if($cookieValid){
 	?>
-			<li><a href="/accountdetails">My Account</a>
+			<li><a href="/accountdetails.php">My Account</a>
 				<ul>
-					<li><a href="/accountdetails">Account Details</a></li>
-					<li><a href="/accountworkers">My Workers</a></li>
-					<li><a href="/accounttransactions">Transaction History</a></li>
+					<li><a href="/accountdetails.php">Account Details</a></li>
+					<li><a href="/accountworkers.php">My Workers</a></li>
+					<li><a href="/accounttransactions.php">Transaction History</a></li>
 				</ul>
 			</li>
 	<?php
 	//If this user is an admin show the adminPanel link
         	if($isAdmin){
 	?>
-			<li><a href="/adminPanel">Admin Panel</a></li>
+			<li><a href="/adminPanel.php">Admin Panel</a></li>
 	<?php
         	}
         }
 	?>
 	<?php if($cookieValid){ ?>
-		<li><a href="/statsAuth">Stats</a>
+		<li><a href="/statsAuth.php">Stats</a>
 			<ul>
-				<li><a href="/statsAuth">Pool Stats</a></li>
-				<li><a href="/blocksAuth">Block Stats</a></li>
+				<li><a href="/statsAuth.php">Pool Stats</a></li>
+				<li><a href="/blocksAuth.php">Block Stats</a></li>
 			</ul>
 		</li>
 	<?php } else { ?>
-		<li><a href="/stats">Stats</a></li>
+		<li><a href="/stats.php">Stats</a></li>
 	<?php } ?>
-		<li><a href="/gettingstarted">Getting Started</a></li>
+		<li><a href="/gettingstarted.php">Getting Started</a></li>
 
-		<li><a href="/support">Support</a></li>
+		<li><a href="/support.php">Support</a></li>
 
-		<li><a href="/about">About</a>
+		<li><a href="/about.php">About</a>
 
-		<li><a href="/news">News</a>
+		<li><a href="/news.php">News</a>
 		<!--
 			<ul>
 				<li><a href="#">About Bitcoin</a></li>
@@ -55,7 +55,7 @@
 		</li>
 
 		<?php if($cookieValid){ ?>
-			<li><a href="/logout">Logout</a></li>
+			<li><a href="/logout.php">Logout</a></li>
 		<?php } ?>
 	</ul>
 </div>
